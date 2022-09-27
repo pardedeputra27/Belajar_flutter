b9e7d12139c800ed3513fef2a511041b44d504ac
pardedeputra27 <pardedeputra27@gmail.com>
2022-09-27 15:04:43 +0700

    menginisialkan routes agar tidak ada routes yang double

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ import 'package:flutter_application_2/Belajar/belajar_media_query.dart';
 import 'package:flutter_application_2/Belajar/belajar_row_dan_column.dart';
 import 'package:flutter_application_2/Belajar/belajar_spacer.dart';
 import 'package:flutter_application_2/Belajar/belajar_statefull.dart';
-
 import 'package:flutter_application_2/Belajar/belajar_textfield.dart';
 import 'package:flutter_application_2/Belajar/belajarlist_dan_listview.dart';
 import 'package:flutter_application_2/Belajar/draggable.dart';
@@ -33,82 +32,105 @@ class MyApp extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              bottom: const TabBar(
-                tabs: [
-                  Tab(icon: Icon(Icons.computer)),
-                  Tab(icon: Icon(Icons.safety_check)),
-                  Tab(icon: Icon(Icons.directions_bike)),
-                ],
-              ),
-              title: const Text('BeLajar dari Erico dermawan'),
-            ),
-            body: TabBarView(children: [
-              ListView(children: [
-                Center(
-                    child: Container(
-                  margin: const EdgeInsets.only(top: 10, bottom: 10),
-                  child: const Text(
-                    'Ini adalah hasil pembelajaran saya',
-                    style: TextStyle(
-                        color: Colors.green, fontSize: 20, letterSpacing: 2),
-                  ),
-                )),
-                containerList(
-                    context, 'Row and Column', const BelajarRowAndColumn()),
-                containerList(context, 'Container', const BelajarContainer()),
-                containerList(context, 'Statefull', const BelajarStatefull()),
-                containerList(context, 'Anynomous Method',
-                    const BelajarAnonymousMethod()),
-                containerList(context, 'Text Style', const BelajarTextStyle()),
-                containerList(context, 'List and ListView',
-                    const BelajarListAndListview()),
-                containerList(context, 'Animated and Gesture',
-                    const BelajarAnimatedAndGesture()),
-                containerList(
-                    context, 'Flexible', const BelajarFlexibleWidget()),
-                containerList(context, 'Stack and Align', const BelajarStack()),
-                containerList(context, 'Image', const BelajarImage()),
-                containerList(context, 'Spacer', const BelajarSpacer()),
-                containerList(context, 'Draggable', const BelajarDraggable()),
-                containerList(context, 'Card', const BelajarCard()),
-                containerList(context, 'Text Field', const BelajarTextField()),
-                containerList(
-                    context, 'Media Query', const BelajarMediaQuery()),
-                containerList(
-                    context, 'inkWell (buttonSendiri)', const BelajarInkwell()),
-                containerList(context, 'CostumCard', const BelajarCostumCard()),
-                containerList(context, 'Login Page', const BelajarLoginPage()),
-                containerList(context, 'HeroAndClipRRect',
-                    const BelajarHeroAndClipRRect()),
-                containerList(
-                    context, 'Appbar design', const BelajarDesignAppBar()),
-              ]),
-              const Icon(
-                Icons.safety_check,
-                size: 200,
-              ),
-              const Icon(
-                Icons.directions_bike,
-                size: 200,
-              ),
-            ])),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const FirstRoutes(),
+          '/row': (context) => const BelajarRowAndColumn(),
+          '/container': (context) => const BelajarContainer(),
+          '/statefull': (context) => const BelajarStatefull(),
+          '/anynomous': (context) => const BelajarAnonymousMethod(),
+          '/textStyle': (context) => const BelajarTextStyle(),
+          '/list': (context) => const BelajarListAndListview(),
+          '/animated': (context) => const BelajarAnimatedAndGesture(),
+          '/flexible': (context) => const BelajarFlexibleWidget(),
+          '/stack': (context) => const BelajarStack(),
+          '/image': (context) => const BelajarImage(),
+          '/spacer': (context) => const BelajarSpacer(),
+          '/draggable': (context) => const BelajarDraggable(),
+          '/card': (context) => const BelajarCard(),
+          '/textField': (context) => const BelajarTextField(),
+          '/mediaQuery': (context) => const BelajarMediaQuery(),
+          '/inkwell': (context) => const BelajarInkwell(),
+          '/costumCard': (context) => const BelajarCostumCard(),
+          '/loginPage': (context) => const BelajarLoginPage(),
+          '/hero': (context) => const BelajarHeroAndClipRRect(),
+          '/appbar': (context) => const BelajarDesignAppBar(),
+        },
       ),
     );
   }
 }
 
-Container containerList(BuildContext context, String text, Widget route) {
+Container containerList(BuildContext context, String text, String route) {
   return Container(
       margin: const EdgeInsets.only(top: 5, left: 20, right: 20),
       child: ElevatedButton(
         child: Text(text),
         onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => route),
-          );
+          Navigator.pushNamed(context, route);
         },
       ));
+}
+
+class FirstRoutes extends StatelessWidget {
+  const FirstRoutes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.computer)),
+              Tab(icon: Icon(Icons.safety_check)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+          title: const Text('BeLajar dari Erico dermawan'),
+        ),
+        body: TabBarView(children: [
+          ListView(children: [
+            Center(
+                child: Container(
+              margin: const EdgeInsets.only(top: 10, bottom: 10),
+              child: const Text(
+                'Ini adalah hasil pembelajaran saya',
+                style: TextStyle(
+                    color: Colors.green, fontSize: 20, letterSpacing: 2),
+              ),
+            )),
+            containerList(context, 'Row and Column', '/row'),
+            containerList(context, 'Container', '/container'),
+            containerList(context, 'Statefull', '/statefull'),
+            containerList(context, 'Anynomous Method', '/anynomous'),
+            containerList(context, 'Text Style', '/textStyle'),
+            containerList(context, 'List and ListView', '/list'),
+            containerList(context, 'Animated and Gesture', '/animated'),
+            containerList(context, 'Flexible', '/flexible'),
+            containerList(context, 'Stack and Align', '/stack'),
+            containerList(context, 'Image', '/image'),
+            containerList(context, 'Spacer', '/spacer'),
+            containerList(context, 'Draggable', '/draggable'),
+            containerList(context, 'Card', '/card'),
+            containerList(context, 'Text Field', '/textField'),
+            containerList(context, 'Media Query', '/mediaQuery'),
+            containerList(context, 'inkWell (buttonSendiri)', '/inkwell'),
+            containerList(context, 'CostumCard', '/costumCard'),
+            containerList(context, 'Login Page', '/loginPage'),
+            containerList(context, 'HeroAndClipRRect', '/hero'),
+            containerList(context, 'Appbar design', '/appbar'),
+          ]),
+          const Icon(
+            Icons.safety_check,
+            size: 200,
+          ),
+          const Icon(
+            Icons.directions_bike,
+            size: 200,
+          ),
+        ]),
+      ),
+    );
+  }
 }
